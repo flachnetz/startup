6ab7bb8f368d6e80b832edd1b8a50501ef6406c3
Martin Joehren <martin.joehren@esailors.de>
2019-03-22 07:34:37 +0000

    adds auth interceptor with tracing

---
+++ lib/tracing/grpc.go
@@ -0,0 +1,69 @@
+package tracing
+
+import (
+	"context"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+
+	grpc_opentracing "github.com/grpc-ecosystem/go-grpc-middleware/tracing/opentracing"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"github.com/modern-go/gls"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+	"google.golang.org/grpc"
+)
+
+type AuthFunc func(ctx context.Context, req interface{}, md metadata.MD) (context.Context, error)
+
+func AuthInterceptor(authFunc AuthFunc) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+
+		meta, _ := metadata.FromIncomingContext(ctx)
+		logrus.WithField("prefix", "auth-interceptor").Debugf("incoming md: %+v", meta)
+		if authContext, err := authFunc(ctx, req, meta); err != nil {
+			return nil, err
+		} else {
+			ctx = authContext
+		}
+		wireContext, err := opentracing.GlobalTracer().Extract(opentracing.TextMap, MdCarrier(meta))
+		if err != nil {
+			logrus.WithField("prefix", "auth-interceptor").Errorf("error %s", err)
+			return handler(ctx, req)
+		}
+
+		span := opentracing.GlobalTracer().StartSpan(info.FullMethod, ext.RPCServerOption(wireContext))
+		defer span.Finish()
+		ctxWithSpan := opentracing.ContextWithSpan(ctx, span)
+
+		gls.WithGls(func() {
+			WithSpan(span, func() {
+				resp, err = handler(opentracing.ContextWithSpan(ctxWithSpan, span), req)
+				s, _ := status.FromError(err)
+				httpCode := runtime.HTTPStatusFromCode(s.Code())
+				defer func() {
+					grpc_opentracing.ClientAddContextTags(ctx, opentracing.Tags{"http.real_status_code": httpCode})
+					ext.HTTPStatusCode.Set(span, uint16(httpCode))
+				}()
+
+			})
+		})()
+
+		return
+
+	}
+}
+
+type MdCarrier metadata.MD
+
+func (mdCarrier MdCarrier) ForeachKey(handler func(key, val string) error) error {
+	md := (metadata.MD)(mdCarrier)
+
+	for key, values := range md {
+		if err := handler(key, values[0]); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
