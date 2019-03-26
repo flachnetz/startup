c16271012ad480cb5884e9c288befd90d17270d6
Oliver Bestmann <obest@esailors.de>
2019-03-26 07:12:27 +0000

    Cleanup tracing library

--- startup_tracing/utils.go
+++ startup_tracing/utils.go
@@ -16,24 +16,10 @@ var activeSpanKey = activeSpan{}
 
 // Returns the current span, or nil, if no span is currently set
 // in local storage.
+//
+// Deprecated: Start using the version with an explicit context parameter.
+//
 func CurrentSpan() opentracing.Span {
-	if g := gls.GetGls(gls.GoID()); g != nil {
-		if span, ok := g[activeSpanKey].(opentracing.Span); ok {
-			return span
-		}
-	}
-
-	return nil
-}
-
-// Returns the current span, or nil, if no span is currently set
-// in local storage.
-func CurrentSpanFromContext(ctx context.Context) opentracing.Span {
-	span := opentracing.SpanFromContext(ctx)
-	if span != nil {
-		return span
-	}
-
 	if g := gls.GetGls(gls.GoID()); g != nil {
 		if span, ok := g[activeSpanKey].(opentracing.Span); ok {
 			return span
@@ -46,6 +32,9 @@ func CurrentSpanFromContext(ctx context.Context) opentracing.Span {
 // Runs the given function with the provided span
 // set in local storage for the duration of function call.
 // This method will not call 'Finish()' on the span
+//
+// Deprecated: Please propagate spans using contexts.
+//
 func WithSpan(span opentracing.Span, fn func()) {
 	if g := gls.GetGls(gls.GoID()); g != nil {
 		previousSpan := g[activeSpanKey]
@@ -63,12 +52,18 @@ func WithSpan(span opentracing.Span, fn func()) {
 
 // Runs an operation and traces it with the given name. This will create a
 // new child span if some span is currently active.
+//
+// Deprecated: propagate spans using context.
+//
 func TraceChild(op string, fn func(span opentracing.Span) error) (err error) {
 	return trace(op, false, fn)
 }
 
 // Runs an operation and traces it with the given name. This will create a
 // new span if no span is currently active.
+//
+// Deprecated: propagate spans using context.
+//
 func TraceOrCreate(op string, fn func(span opentracing.Span) error) (err error) {
 	return trace(op, true, fn)
 }
@@ -109,7 +104,7 @@ func trace(op string, always bool, fn func(span opentracing.Span) error) (err er
 
 // Trace a child call while propagating the span using the context.
 func TraceChildContext(ctx context.Context, op string, fn func(ctx context.Context, span opentracing.Span) error) (err error) {
-	parentSpan := opentracing.SpanFromContext(ctx)
+	parentSpan := spanFromContextOrGLS(ctx)
 	if parentSpan == nil {
 		return fn(ctx, noopSpan)
 	}
@@ -129,4 +124,22 @@ func TraceChildContext(ctx context.Context, op string, fn func(ctx context.Conte
 
 	err = fn(opentracing.ContextWithSpan(ctx, span), span)
 	return
+}
+
+// Returns the current span, or nil, if no span is currently set
+// in local storage.
+//
+func spanFromContextOrGLS(ctx context.Context) opentracing.Span {
+	span := opentracing.SpanFromContext(ctx)
+	if span != nil {
+		return span
+	}
+
+	if g := gls.GetGls(gls.GoID()); g != nil {
+		if span, ok := g[activeSpanKey].(opentracing.Span); ok {
+			return span
+		}
+	}
+
+	return nil
 }
