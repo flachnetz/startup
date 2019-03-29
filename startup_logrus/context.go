04a6e4140c6872cf12305cc4205018139693fb05
Oliver Bestmann <obest@esailors.de>
2019-03-29 06:37:06 +0000

    Add logger functions

---
+++ startup_logrus/context.go
@@ -0,0 +1,23 @@
+package startup_logrus
+
+import (
+	"context"
+	"github.com/sirupsen/logrus"
+)
+
+type loggerKey struct{}
+
+// stores the entry into the context so it can be retrieved with GetLogger later.
+func WithLogger(ctx context.Context, logger *logrus.Entry) context.Context {
+	return context.WithValue(ctx, loggerKey{}, logger)
+}
+
+// returns the current logger with the given prefix from the context.
+func GetLogger(ctx context.Context, prefix string) *logrus.Entry {
+	logger := ctx.Value(loggerKey{})
+	if logger == nil {
+		return emptyEntry.WithField("prefix", prefix)
+	}
+
+	return logger.(*logrus.Entry).WithField("prefix", prefix)
+}
---
+++ startup_logrus/trace.go
@@ -0,0 +1,75 @@
+package startup_logrus
+
+import (
+	"context"
+	"github.com/opentracing/opentracing-go"
+	"github.com/sirupsen/logrus"
+	"reflect"
+)
+
+// default empty logger
+var emptyEntry = logrus.NewEntry(logrus.StandardLogger())
+
+type h struct{}
+
+func NewTracingHook() logrus.Hook {
+	return h{}
+}
+
+func (h) Levels() []logrus.Level {
+	return logrus.AllLevels
+}
+
+func (h) Fire(entry *logrus.Entry) error {
+	if entry.Context == nil {
+		return nil
+	}
+
+	// get the span from the entries context
+	spanContext := spanContextOf(entry.Context)
+	if spanContext == nil {
+		return nil
+	}
+
+	if entry.Data == nil {
+		entry.Data = logrus.Fields{}
+	}
+
+	if traceId := traceIdOf(spanContext); traceId != "" {
+		// we can not create a new entry here, as logrus does not support "chaining" of hooks,
+		// so we will just modify the existing entry here before logging
+		entry.Data["traceId"] = traceId
+	}
+
+	return nil
+}
+
+func spanContextOf(ctx context.Context) opentracing.SpanContext {
+	span := opentracing.SpanFromContext(ctx)
+	if span == nil {
+		return nil
+	}
+
+	return span.Context()
+}
+
+func traceIdOf(spanContext opentracing.SpanContext) string {
+	type hexer interface{ ToHex() string }
+
+	rSpanContext := reflect.ValueOf(spanContext).Elem()
+	if !rSpanContext.IsValid() || rSpanContext.Kind() != reflect.Struct {
+		return ""
+	}
+
+	fieldTraceId := rSpanContext.FieldByName("TraceID")
+	if !fieldTraceId.IsValid() {
+		return ""
+	}
+
+	traceId, ok := fieldTraceId.Interface().(hexer)
+	if ! ok {
+		return ""
+	}
+
+	return traceId.ToHex()
+}
