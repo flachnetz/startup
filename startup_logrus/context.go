6d02e8cbea2fe27169cf6485c850c71a16d66b75
Oliver Bestmann <obest@esailors.de>
2019-03-29 07:03:01 +0000

    Improve GetLogger

--- startup_logrus/context.go
+++ startup_logrus/context.go
@@ -2,6 +2,7 @@ package startup_logrus
 
 import (
 	"context"
+	"fmt"
 	"github.com/sirupsen/logrus"
 	"reflect"
 )
@@ -13,27 +14,47 @@ func WithLogger(ctx context.Context, logger *logrus.Entry) context.Context {
 	return context.WithValue(ctx, loggerKey{}, logger)
 }
 
-// returns the current logger with the given prefix from the context.
-func GetLogger(ctx context.Context, prefix string) *logrus.Entry {
+// returns the current logger with the given prefix or type name from the context.
+func GetLogger(ctx context.Context, object interface{}) *logrus.Entry {
+	log := loggerOf(ctx)
+
+	prefix := prefixOf(object)
+	if prefix == "" {
+		return log
+	}
+
+	return log.WithField("prefix", prefix)
+}
+
+func loggerOf(ctx context.Context) *logrus.Entry {
 	if ctx == nil {
-		return emptyEntry.WithField("prefix", prefix)
+		return emptyEntry
 	}
 
 	logger := ctx.Value(loggerKey{})
 	if logger == nil {
-		return emptyEntry.WithField("prefix", prefix)
+		return emptyEntry
 	}
 
-	return logger.(*logrus.Entry).WithField("prefix", prefix)
+	return logger.(*logrus.Entry)
 }
 
-func GetLoggerForObject(ctx context.Context, object interface{}) *logrus.Entry {
-	t := reflect.ValueOf(object).Type()
+func prefixOf(object interface{}) string {
+	switch object := object.(type) {
+	case string:
+		return object
 
-	prefix := t.Name()
-	if prefix == "" {
-		prefix = t.PkgPath()
+	case fmt.Stringer:
+		return object.String()
+
+	default:
+		t := reflect.ValueOf(object).Type()
+
+		prefix := t.Name()
+		if prefix == "" {
+			prefix = t.PkgPath()
+		}
+
+		return prefix
 	}
-
-	return GetLogger(ctx, prefix)
 }
