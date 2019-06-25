d23c91de23b9119857d40ce074b72016135f823f
Oliver Bestmann <oliver.bestmann@googlemail.com>
2019-06-25 13:46:54 +0000

    Add skip function input argument

--- startup_tracing_pg/tracing.go
+++ startup_tracing_pg/tracing.go
@@ -15,6 +15,12 @@ import (
 )
 
 type PostgresTracingOptions struct {
+	Inputs struct {
+		// Extra skip function that skips methods (by name) when guessing the
+		// transaction span tags.
+		SkipFrameworkMethod func(name string) bool
+	}
+
 	once sync.Once
 }
 
@@ -33,13 +39,20 @@ func (opts *PostgresTracingOptions) Initialize(tops *startup_tracing.TracingOpti
 			sql.Register("pgx", sqlhooks.Wrap(&pq.Driver{}, &dbHook{tops.Inputs.ServiceName + "-db"}))
 
 			// replace the new transaction function with a new hook
-			installTransactionTracingHook(tops.Inputs.ServiceName + "-db")
+			opts.installTransactionTracingHook(tops.Inputs.ServiceName + "-db")
 		}
 	})
 }
 
-func installTransactionTracingHook(serviceName string) {
+func (opts *PostgresTracingOptions) installTransactionTracingHook(serviceName string) {
 	withTransactionContext := WithTransactionContext
+
+	skipFunction := opts.Inputs.SkipFrameworkMethod
+	if skipFunction == nil {
+		skipFunction = func(name string) bool {
+			return false
+		}
+	}
 
 	WithTransactionContext = func(ctx context.Context, db TxStarter, operation TransactionFn) (err error) {
 		var tag string
@@ -53,7 +66,7 @@ func installTransactionTracingHook(serviceName string) {
 				frame, more := frames.Next()
 
 				// take first one out of startup
-				if !strings.Contains(frame.Function, "flachnetz/startup/") {
+				if !strings.Contains(frame.Function, "flachnetz/startup/") && !skipFunction(frame.Function) {
 					tag = frame.Function
 					break
 				}
