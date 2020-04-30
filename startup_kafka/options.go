d478e4596612460122a1e4edc78bf8bbcc137dbc
Martin Joehren <m.joehren@gmail.com>
2020-04-30 07:01:18 +0000

    better defaults for kafka client

--- startup_kafka/options.go
+++ startup_kafka/options.go
@@ -66,14 +66,31 @@ func (opts *KafkaOptions) KafkaClient() sarama.Client {
 
 func defaultConfig() *sarama.Config {
 	config := sarama.NewConfig()
-	config.Net.MaxOpenRequests = 16
-	config.Net.DialTimeout = 10 * time.Second
+
+	config.Version = sarama.V2_4_0_0
+
 	config.Consumer.MaxWaitTime = 8 * time.Second
 	config.Consumer.Fetch.Min = 1024
 	config.Consumer.Return.Errors = true
-	config.ChannelBufferSize = 4
+
+	config.Net.MaxOpenRequests = 16
+	config.Net.DialTimeout = 10 * time.Second
+	config.Net.KeepAlive = 1
 	config.Net.TLS.Enable = true
 
-	config.Version = sarama.V2_4_0_0
+	config.Metadata.Retry.Max = 60
+	config.Metadata.Retry.Backoff = 2000 * time.Millisecond
+
+	config.Producer.Timeout = 3 * time.Second
+	config.Producer.Idempotent = false
+
+	config.Producer.Retry.Max = 60
+	config.Producer.Retry.Backoff = 2000 * time.Millisecond
+
+	config.Producer.Flush.Bytes = 64000
+	config.Producer.Flush.Frequency = 1000 * time.Millisecond
+	config.Producer.Flush.MaxMessages = 100
+	config.Producer.Flush.Messages = 100
+
 	return config
 }
