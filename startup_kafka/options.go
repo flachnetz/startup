6165085b6759b1e8219b6889e9be50bc8654a5d0
Oliver Bestmann <oliver.bestmann@googlemail.com>
2021-09-13 07:45:23 +0200

    Expose a method to create a kafka instance

--- startup_kafka/options.go
+++ startup_kafka/options.go
@@ -1,10 +1,43 @@
 package startup_kafka
 
+import (
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/flachnetz/startup/v2/startup_base"
+	"strings"
+	"time"
+)
+
 // Simple flags for consumer
 type KafkaOptions struct {
 	KafkaAddresses     []string `long:"kafka-address" validate:"dive,hostport" description:"Address of kafka server to use. Can be specified multiple times to connect to multiple brokers."`
-	KafkaConsumerGroup string   `long:"kafka-consumer-group" default:"abconfig" description:"Consumer group of kafka messages"`
+	KafkaConsumerGroup string   `long:"kafka-consumer-group" default:"RANDOM" description:"Consumer group of kafka messages"`
 	KafkaOffsetReset   string   `long:"kafka-offset-reset" default:"smallest" description:"Offset reset for kafka topic" choice:"smallest" choice:"largest"`
 }
 
+func (opts KafkaOptions) NewConsumer(config kafka.ConfigMap) *kafka.Consumer {
+	if opts.KafkaConsumerGroup == "RANDOM" {
+		opts.KafkaConsumerGroup = fmt.Sprintf("golang-%d", time.Now().UnixNano())
+	}
 
+	configMap := kafka.ConfigMap{
+		"bootstrap.servers": strings.Join(opts.KafkaAddresses, ","),
+		"group.id":          opts.KafkaConsumerGroup,
+		"auto.offset.reset": opts.KafkaOffsetReset,
+
+		// use ssl by default
+		"security.protocol": "ssl",
+
+		// auto commit true is the default.
+		"enable.auto.commit": "false",
+	}
+
+	for key, value := range config {
+		configMap[key] = value
+	}
+
+	consumer, err := kafka.NewConsumer(&configMap)
+	startup_base.FatalOnError(err, "create kafka consumer failed")
+
+	return consumer
+}
