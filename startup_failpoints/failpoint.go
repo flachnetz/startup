d3135830fcef153abf0593cfbf68482ad29014c1
Martin Joehren <martin.joehren@esailors.de>
2022-06-20 15:06:49 +0200

    feature: failpoint sort

--- startup_failpoints/failpoint.go
+++ startup_failpoints/failpoint.go
@@ -28,9 +28,9 @@ func (t timeoutError) Error() string {
 }
 
 var timeoutErrors = []FailPointError{
-	timeoutError{"timeout_1_second", 1 * time.Second, false},
-	timeoutError{"timeout_3_seconds", 3 * time.Second, false},
-	timeoutError{"timeout_5_seconds", 5 * time.Second, false},
+	timeoutError{"timeout_01_second", 1 * time.Second, false},
+	timeoutError{"timeout_03_seconds", 3 * time.Second, false},
+	timeoutError{"timeout_05_seconds", 5 * time.Second, false},
 	timeoutError{"timeout_10_seconds", 10 * time.Second, false},
 	timeoutError{"timeout_30_seconds", 30 * time.Second, false},
 	timeoutError{"timeout_forever", 365 * 24 * time.Hour, true},
