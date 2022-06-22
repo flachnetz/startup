454c79f98f49a91034e1d267199009450ea42e23
Martin Joehren <martin.joehren@esailors.de>
2022-06-22 10:41:41 +0200

    feature: logger context with fields

--- startup_logrus/context.go
+++ startup_logrus/context.go
@@ -11,12 +11,12 @@ import (
 
 type loggerKey struct{}
 
-// stores the entry into the context so it can be retrieved with GetLogger later.
+// WithLogger stores the entry into the context so it can be retrieved with GetLogger later.
 func WithLogger(ctx context.Context, logger *logrus.Entry) context.Context {
 	return context.WithValue(ctx, loggerKey{}, logger)
 }
 
-// returns the current logger with the given prefix or type name from the context.
+// GetLogger returns the current logger with the given prefix or type name from the context.
 func GetLogger(ctx context.Context, object interface{}) *logrus.Entry {
 	log := loggerOf(ctx).WithContext(ctx)
 
@@ -26,6 +26,21 @@ func GetLogger(ctx context.Context, object interface{}) *logrus.Entry {
 	}
 
 	return log.WithField("prefix", prefix)
+}
+
+func ContextLoggerWithFields(ctx context.Context, fields ...string) context.Context {
+	logger := GetLoggerWithFields(ctx, fields...)
+	return WithLogger(ctx, logger)
+}
+
+func GetLoggerWithFields(ctx context.Context, fields ...string) *logrus.Entry {
+	logger := GetLogger(ctx, "")
+	for i := 0; i < len(fields)-1; i += 2 {
+		k := fields[i]
+		v := fields[i+1]
+		logger = logger.WithField(k, v)
+	}
+	return logger
 }
 
 func loggerOf(ctx context.Context) *logrus.Entry {
