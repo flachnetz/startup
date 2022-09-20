294efc5730a3005e925ce95f705a69f9c73a055c
Martin Joehren <martin.joehren@esailors.de>
2022-09-20 18:26:53 +0200

    feat: migration takes schema as argument

--- startup_postgres/migrations.go
+++ startup_postgres/migrations.go
@@ -9,15 +9,16 @@ import (
 	"os"
 )
 
-// Runs a migration with the sql files from the given directory.
+// Migration Runs a migration with the sql files from the given directory.
 // The directory must exist. The migration library will use the given table
 // name to store the migration progress
-func Migration(table, directory string) Initializer {
+func Migration(schema, table, directory string) Initializer {
 	if _, err := os.Stat(directory); os.IsNotExist(err) {
 		startup_base.PanicOnError(err, "No database migration files found")
 	}
 
 	return func(db *sqlx.DB) error {
+		migrate.SetSchema(schema)
 		migrate.SetTable(table)
 
 		migrations := &migrate.FileMigrationSource{Dir: directory}
