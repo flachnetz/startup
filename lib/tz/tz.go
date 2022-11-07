9206d0383ab71a17e65d7fdc40dfde83b2331750
Oliver Bestmann <obest@esailors.de>
2022-11-07 11:41:14 +0100

    Fix timezone Europe/Berlin

--- lib/tz/tz.go
+++ lib/tz/tz.go
@@ -10,7 +10,7 @@ var EuropeBerlin = MustLoadLocation("Europe/Berlin")
 var UTC = MustLoadLocation("UTC")
 
 func MustLoadLocation(name string) *time.Location {
-	loc, err := time.LoadLocation("UTC")
+	loc, err := time.LoadLocation(name)
 	startup_base.PanicOnError(err, "load timezone %q", name)
 
 	return loc
