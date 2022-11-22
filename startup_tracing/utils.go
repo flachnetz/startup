10d3333ab88c17f464ac65f1199b8047eedc21e7
Martin Joehren <martin.joehren@esailors.de>
2022-11-22 11:13:23 +0100

    simple trace with not result

--- startup_tracing/utils.go
+++ startup_tracing/utils.go
@@ -9,6 +9,13 @@ import (
 	"github.com/pkg/errors"
 )
 
+func SimpleTrace(ctx context.Context, op string, fn func(ctx context.Context, span opentracing.Span) error) (err error) {
+	_, err = Trace(ctx, op, func(ctx context.Context, span opentracing.Span) (any, error) {
+		return nil, fn(ctx, span)
+	})
+	return err
+}
+
 // Trace traces a child call while propagating the span using the context.
 func Trace[T any](ctx context.Context, op string, fn func(ctx context.Context, span opentracing.Span) (T, error)) (result T, err error) {
 	var parentContext opentracing.SpanContext
