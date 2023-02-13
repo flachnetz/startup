5d978c562352da483dcca3c5b1f5a3dc13afd292
Martin Joehren <martin.joehren@esailors.de>
2023-02-13 18:11:42 +0100

    [feat] unleash init func

---
+++ lib/unleash/unleash.go
@@ -0,0 +1,26 @@
+package unleash
+
+import (
+	"github.com/Unleash/unleash-client-go/v3"
+	"github.com/flachnetz/startup/v2"
+	"github.com/flachnetz/startup/v2/startup_base"
+	"github.com/flachnetz/startup/v2/startup_tracing"
+	"net/http"
+	"net/url"
+)
+
+type Unleash struct {
+	BaseUrl  startup.URL `long:"unleash-base-url" default:"http://unleash.shared.svc.cluster.local/api" description:"Unleash base URL"`
+	ApiToken string      `long:"unleash-api-token" default:"default:production.unleash-insecure-api-token" description:"Unleash API token"`
+}
+
+func InitUnleash(appName string, unleashUrl *url.URL, token string, listener interface{}) {
+	err := unleash.Initialize(
+		unleash.WithListener(listener),
+		unleash.WithAppName(appName),
+		unleash.WithUrl(unleashUrl.String()),
+		unleash.WithCustomHeaders(http.Header{"Authorization": {token}}),
+		unleash.WithHttpClient(startup_tracing.WithSpanPropagation(&http.Client{})),
+	)
+	startup_base.PanicOnError(err, "init unleash")
+}
