3fe22afa7be5628daffc2cbe0d99a7a8c1f83715
Oliver Bestmann <obest@esailors.de>
2023-11-28 10:42:32 +0100

    fix: remove echo middleware, logging should handle this automatically.

--- startup_logrus/trace.go
+++ startup_logrus/trace.go
@@ -4,21 +4,9 @@ import (
 	"context"
 	"log/slog"
 
-	"github.com/labstack/echo/v4"
 	"github.com/opentracing/opentracing-go"
 	zipkintracer "github.com/openzipkin-contrib/zipkin-go-opentracing"
 )
-
-func EchoTraceLoggerMiddleware() echo.MiddlewareFunc {
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			traceId := c.Request().Header.Get("X-Trace-Id")
-			ctx := ContextLoggerWithFields(c.Request().Context(), "traceId", traceId)
-			c.SetRequest(c.Request().Clone(ctx))
-			return next(c)
-		}
-	}
-}
 
 func WithTraceId(ctx context.Context, record slog.Record) (slog.Record, bool, error) {
 	// get the span from the entries context
