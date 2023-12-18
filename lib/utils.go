0a0e45091e81376b7e4562efd76377529d7cea1f
Oliver Bestmann <obest@esailors.de>
2023-12-18 12:41:58 +0100

    feat: add no cancel context

--- lib/utils.go
+++ lib/utils.go
@@ -1,6 +1,9 @@
 package lib
 
-import "golang.org/x/exp/constraints"
+import (
+	"context"
+	"golang.org/x/exp/constraints"
+)
 
 func PtrOf[T any](value T) *T {
 	return &value
@@ -18,4 +21,20 @@ func Max[T constraints.Ordered](a, b T) T {
 		return a
 	}
 	return b
+}
+
+type noCancelContext struct {
+	context.Context
+	lookupContext context.Context
+}
+
+// NoCancelContext creates a new context that derives from context.Background, while getting
+// all values from the given lookupContext. So even if the lookupContext has a timeout, the
+// new context will not inherit this timeout.
+func NoCancelContext(lookupContext context.Context) context.Context {
+	return &noCancelContext{context.Background(), lookupContext}
+}
+
+func (ctx *noCancelContext) Value(key interface{}) interface{} {
+	return ctx.lookupContext.Value(key)
 }
