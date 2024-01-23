1e3f619d7070bf4d02e5a0fda8832a2bd22c26c6
Martin Joehren <martin.joehren@esailors.de>
2024-01-23 16:04:13 +0100

    feat: valuer for slice

--- lib/ql/array.go
+++ lib/ql/array.go
@@ -1,12 +1,20 @@
 package ql
 
 import (
+	"database/sql/driver"
 	"github.com/jackc/pgx/v5/pgtype"
 )
 
 var pgtypeMap = pgtype.NewMap()
 
 type StringArray []string
+
+func (s *StringArray) Value() (driver.Value, error) {
+	if s == nil || len(*s) == 0 {
+		return nil, nil
+	}
+	return s, nil
+}
 
 func (s *StringArray) Scan(src any) error {
 	return pgtypeMap.SQLScanner((*[]string)(s)).Scan(src)
