fe87fadfa3ec39c40c4f238e77d6035effcc5072
Martin Joehren <martin.joehren@esailors.de>
2024-02-01 18:31:25 +0100

    feat: http status err from api error

--- lib/api/echo.go
+++ lib/api/echo.go
@@ -10,41 +10,41 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func CustomErrorHandler(errorHandler ErrorHandler) func(error, echo.Context) {
+func CustomErrorHandler[E error](errorHandler ErrorHandler[E]) func(error, echo.Context) {
 	return func(err error, c echo.Context) {
 		errorHandler.HandleError(c.Request().Context(), c, err)
 	}
 }
 
-type ErrorHandler struct {
+type ErrorHandler[E error] struct {
 	UnknownError   func(msg string) error
 	TimeoutError   func(msg string) error
 	HttpStatusFrom func(ctx context.Context, err error) int
-	ToApiError     func(err error) error
+	ToApiError     func(err error) E
 }
 
-func (eh *ErrorHandler) toApiError(err error) error {
+func (eh *ErrorHandler[E]) toApiError(err error) error {
 	if eh.ToApiError != nil {
 		return eh.ToApiError(err)
 	}
 	return ErrUnknown.WithDescription(err.Error())
 }
 
-func (eh *ErrorHandler) timeoutError(msg string) error {
+func (eh *ErrorHandler[E]) timeoutError(msg string) error {
 	if eh.TimeoutError != nil {
 		return eh.TimeoutError(msg)
 	}
 	return ErrTimeout.WithDescription(msg)
 }
 
-func (eh *ErrorHandler) unknownError(msg string) error {
+func (eh *ErrorHandler[E]) unknownError(msg string) error {
 	if eh.UnknownError != nil {
 		return errors.New(msg)
 	}
 	return ErrUnknown
 }
 
-func (eh *ErrorHandler) httpStatusFrom(ctx context.Context, err error) int {
+func (eh *ErrorHandler[E]) httpStatusFrom(ctx context.Context, err error) int {
 	if eh.HttpStatusFrom != nil {
 		return eh.HttpStatusFrom(ctx, err)
 	}
@@ -56,10 +56,10 @@ func (eh *ErrorHandler) httpStatusFrom(ctx context.Context, err error) int {
 	return httpStatusFrom
 }
 
-func (eh *ErrorHandler) HandleError(ctx context.Context, c echo.Context, err error) {
+func (eh *ErrorHandler[E]) HandleError(ctx context.Context, c echo.Context, err error) {
 	logger := startup_logrus.GetLogger(ctx, "HandleError")
 	apiError := eh.toApiError(err)
-	httpStatusFrom := eh.httpStatusFrom(ctx, err)
+	httpStatusFrom := eh.httpStatusFrom(ctx, apiError)
 	if httpStatusFrom == 499 {
 		apiError = eh.timeoutError(apiError.Error())
 	}
