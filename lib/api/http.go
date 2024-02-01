1d9957d3a006449570d71bdcb0776f5e0582d14a
Martin Joehren <martin.joehren@esailors.de>
2024-02-01 18:48:20 +0100

    fix: site id tolower and toupper

--- lib/api/http.go
+++ lib/api/http.go
@@ -1,15 +1,18 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 type SiteId string
 
 func (s SiteId) ToLower() SiteId {
-	return SiteId(s.ToLower())
+	return SiteId(strings.ToLower(string(s)))
 }
 
 func (s SiteId) ToUpper() SiteId {
-	return SiteId(s.ToUpper())
+	return SiteId(strings.ToUpper(string(s)))
 }
 
 func GetSiteFromHeader(header http.Header) (SiteId, error) {
