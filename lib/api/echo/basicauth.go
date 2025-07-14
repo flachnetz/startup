3f2fdd5dc820c0acf121ac3c38545e3cec816c39
Martin Joehren <martin.joehren@zealnetwork.de>
2025-07-14 16:25:01 +0200

    feat: echo rest users option

--- lib/api/echo/basicauth.go
+++ lib/api/echo/basicauth.go
@@ -5,9 +5,22 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+type RestUsers struct {
+	RestUser map[string]string `long:"http-rest-user" description:"Usernames for REST API access."`
+}
+
 func BasicAuthValidator(basicAuthUser string, basicAuthPassword string) middleware.BasicAuthValidator {
 	return func(user string, password string, context echo.Context) (bool, error) {
 		if user == basicAuthUser && password == basicAuthPassword {
+			return true, nil
+		}
+		return false, nil
+	}
+}
+
+func BasicAuthUsersValidator(users map[string]string) middleware.BasicAuthValidator {
+	return func(user string, password string, context echo.Context) (bool, error) {
+		if pass, ok := users[user]; ok && pass == password {
 			return true, nil
 		}
 		return false, nil
