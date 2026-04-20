1b75f50f42d07ef3f903918109e8cc1001f372bd
Oliver Bestmann <oliver.bestmann.contractor@bauermedia.co.uk>
2026-04-20 08:09:23 +0200

    slog: create a slog compatible utility package

---
+++ startup_logging/middleware.go
@@ -0,0 +1,45 @@
+package sl
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+)
+
+type Middleware func(context.Context, slog.Record) (slog.Record, bool, error)
+
+type wrapped struct {
+	delegate   slog.Handler
+	middleware Middleware
+}
+
+func Wrap(handler slog.Handler, middleware Middleware) slog.Handler {
+	return wrapped{delegate: handler, middleware: middleware}
+}
+
+func (w wrapped) Enabled(ctx context.Context, level slog.Level) bool {
+	return w.delegate.Enabled(ctx, level)
+}
+
+func (w wrapped) WithAttrs(attrs []slog.Attr) slog.Handler {
+	w.delegate = w.delegate.WithAttrs(attrs)
+	return w
+}
+
+func (w wrapped) WithGroup(name string) slog.Handler {
+	w.delegate = w.delegate.WithGroup(name)
+	return w
+}
+
+func (w wrapped) Handle(ctx context.Context, record slog.Record) error {
+	record, ok, err := w.middleware(ctx, record)
+	if err != nil {
+		return fmt.Errorf("apply middleware: %w", err)
+	}
+
+	if !ok {
+		return nil
+	}
+
+	return w.delegate.Handle(ctx, record)
+}
