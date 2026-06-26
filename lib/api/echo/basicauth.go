4f45b78d1ce584dae172e71f1887516779f68177
Oliver Bestmann <oliver.bestmann.contractor@bauermedia.co.uk>
2026-06-26 15:02:08 +0200

    Rework api.Error

--- lib/api/echo/basicauth.go
+++ lib/api/echo/basicauth.go
@@ -5,22 +5,25 @@ import (
 	"github.com/labstack/echo/v5/middleware"
 )
 
+type User string
+type Password string
+
 type RestUsers struct {
-	RestUser map[string]string `long:"http-rest-user" env:"HTTP_REST_USER" description:"Usernames for REST API access."`
+	RestUser map[User]Password `long:"http-rest-user" env:"HTTP_REST_USER" description:"Usernames for REST API access."`
 }
 
-func BasicAuthValidator(basicAuthUser string, basicAuthPassword string) middleware.BasicAuthValidator {
-	return func(c *echo.Context, user string, password string) (bool, error) {
-		if user == basicAuthUser && password == basicAuthPassword {
+func BasicAuthValidator(user User, password Password) middleware.BasicAuthValidator {
+	return func(c *echo.Context, inUser string, inPassword string) (bool, error) {
+		if inUser == string(user) && inPassword == string(password) {
 			return true, nil
 		}
 		return false, nil
 	}
 }
 
-func BasicAuthUsersValidator(users map[string]string) middleware.BasicAuthValidator {
+func BasicAuthUsersValidator(users map[User]Password) middleware.BasicAuthValidator {
 	return func(c *echo.Context, user string, password string) (bool, error) {
-		if pass, ok := users[user]; ok && pass == password {
+		if pass, ok := users[User(user)]; ok && string(pass) == password {
 			return true, nil
 		}
 		return false, nil
